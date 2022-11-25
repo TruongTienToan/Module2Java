37e65c0c38e625cc73e0c80b548c199daa5462fe
TruongTienToan <truongtientoan2308@gmail.com>
2022-11-25 09:09:22 +0700

    Lớp Point và MoveablePoint

---
+++ src/main/java/S6_Inheritance/Point_And_MoveablePoint/MoveablePoint.java
@@ -0,0 +1,66 @@
+package S6_Inheritance.Point_And_MoveablePoint;
+
+public class MoveablePoint extends Point {
+    private float xSpeed = 0.0f;
+    private float ySpeed = 0.0f;
+
+    public MoveablePoint() {
+
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x,y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        float[] array = {xSpeed, ySpeed};
+        return array;
+    }
+
+    @Override
+    public String toString() {
+        return "MoveablePoint{"
+                + "xSpeed="
+                + xSpeed
+                + ", ySpeed="
+                + ySpeed
+                + "}";
+    }
+
+    public MoveablePoint move (){
+        super.setX(super.getX() + xSpeed);
+        super.setY(super.getY() + ySpeed);
+        return this;
+    }
+
+    public static void main(String[] args) {
+        MoveablePoint mvp = new MoveablePoint();
+        System.out.println(mvp);
+
+        mvp = new MoveablePoint(10.5f,4,32.5f,12.8f);
+        System.out.println(mvp);
+    }
+}
