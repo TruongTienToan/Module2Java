300d8287d5e202ef9f06af795cdf86513564bef8
TruongTienToan <truongtientoan2308@gmail.com>
2023-01-31 09:06:09 +0700

    Sắp xếp với Comparable và Comparator

---
+++ src/main/java/S12_Java_Collection_Framework/ComparableAndComparator/Main.java
@@ -0,0 +1,31 @@
+package S12_Java_Collection_Framework.ComparableAndComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Kien", 30, "HT");
+        Student student1 = new Student("Nam", 26, "HN");
+        Student student2 = new Student("Trung", 29, "QB");
+        Student student3 = new Student("Tu", 23, "HCM");
+
+        List<Student> lists = new ArrayList<>();
+        lists.add(student);
+        lists.add(student1);
+        lists.add(student2);
+        lists.add(student3);
+        Collections.sort(lists);
+        for (Student st : lists) {
+            System.out.println(st.toString());
+        }
+
+        AgeComparator ageComparator = new AgeComparator();
+        Collections.sort(lists, ageComparator);
+        System.out.println("So sanh theo tuoi:");
+        for (Student st : lists) {
+            System.out.println(st.toString());
+        }
+    }
+}
